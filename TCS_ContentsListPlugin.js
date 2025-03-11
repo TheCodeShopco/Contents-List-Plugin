66f61d1feb47950012be48685ff9b50cc1527008
Alistair Everington <hello@thecodeshop.co>
2025-03-11 10:54:32 +0000

    Add files via upload

--- TCS_ContentsListPlugin.js
+++ TCS_ContentsListPlugin.js
@@ -24,7 +24,7 @@ function assignIdentity(headings) {
 
 function checkForCodeBlock() {
     let contentsBlock = document.getElementById('contents');
-    if (contentsBlock === 'undefined' || contentsBlock === null) {
+    if (contentsBlock === undefined || contentsBlock === null) {
         let blogItemContent = document.querySelector('.blog-item-content');
         if (blogItemContent) {
         contentsBlock = document.createElement('div');
